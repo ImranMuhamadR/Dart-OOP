820df01cd8e9ed301713ed0be01425880c7457f5
ImranMuhamadR <imranmuhamadrafi7@gmail.com>
2023-02-23 21:35:55 +0700

    Menambahkan komentar pada file redirecting_constructor.dart

---
+++ bin/11_redirecting_constructor.dart
@@ -0,0 +1,7 @@
+/*
+!REDIRECTING_CONSTRUCTOR
+- Saat membuat Named Constructor, kita bisa memanggil Default Constructor, instilahnya adalah melakukan redirecting Constructor
+- Cara membuat Redirecting Constructor adalah dengan menambahkan : (titk dua), lalu di ikuti dengan 
+  memanggil this(parameter), dimana this() disini adalah dianggap menagkses Default Constructor
+- Saat membuat Redirecting Constructor, kita tidak bisa menambahkan body pada Redirecting Constructor
+ */
