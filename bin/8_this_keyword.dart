1cfedeca2324c142f987061d750d93407b32a2a6
ImranMuhamadR <imranmuhamadrafi7@gmail.com>
2023-02-23 16:30:16 +0700

    Menambahkan beberapa file pada file this_keyword.dart

--- bin/8_this_keyword.dart
+++ bin/8_this_keyword.dart
@@ -1,8 +1,8 @@
 /*
 !THIS KEYWORD
 - Saat kita membuat kode didalam block constructor atatu method di dalam class, kita bisa menggunakan kata kunci this untuk mengakses object saat ini
-- Misal kadang kita ubtuh mengakses sebuat field yang namanya sma dengan parameter method, hal ini tidak bisa dilakukan jika langsung menyebut nama field, kita bisa mengakses nama field tersebut dengan kata kunci this
-- This juga tidak hanay digunakan untuk mengakses field milik object saat ini, namun juga bisa diguanakan mnegakses method
+- Misal kadang kita ubtuh mengakses sebuat properti yang namanya sama dengan parameter method, hal ini tidak bisa dilakukan jika langsung menyebut nama properti, kita bisa mengakses nama properti tersebut dengan kata kunci this
+- This juga tidak hanya digunakan untuk mengakses properti milik object saat ini, namun juga bisa diguanakan mnegakses method
 - This bisa digunakan untuk mengatasi masalah variable shadowing
  */
 class Person {
@@ -11,7 +11,19 @@ class Person {
   final String country = "Indonesia";
 
   Person(String name, String address) {
+    //INI MERUPAKAN CONTOH DARI THIS KEYWORD
+    /*
+    Penjelasan :
+    jika kita ingin mengakses sebuah properti diatas yang berada di dalam class yaitu (name), (address) 
+    kita bisa menggunakan kata kunci This.(namaProperti nya).
+     */
     this.name = name;
-    this.name = address;
+    this.address = address;
   }
+}
+
+void main() {
+  var person = Person("Imran", "Cirebon");
+  print(person.name);
+  print(person.address);
 }
