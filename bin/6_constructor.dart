f1425e7b43067d837fbb2ea90346499970c76290
ImranMuhamadR <imranmuhamadrafi7@gmail.com>
2023-02-23 16:12:03 +0700

    mengedit beberapa file pada file constructor.dart

--- bin/6_constructor.dart
+++ bin/6_constructor.dart
@@ -1,7 +1,7 @@
 /*
 !CONSTRUCTOR
 - Saat kita membuat object, maka kita seperti membanggil seubah method, karena kita menggunakan kurung()
-- Didalam class, kita bisa membuat constructor, contructor adalah method yang adakan dipanggil saat pertama kali object dibuat
+- Didalam class, kita bisa membuat constructor, contructor adalah method yang akan dipanggil saat pertama kali object dibuat
 - Mirip seperti di method, kita bisa memberi parameter pada constructor
 - Nama Constructor harus sama dengan nama class, dan tidak membutuhkan kata kunci void atau return value
 - Ketika kita menambahkan Constructor pada class, maka saat membuat object baru, kita wajib mengikuti parameter yang ada di Constructor
@@ -14,6 +14,7 @@ class Person {
   String? address;
   final String country = "Indonesia";
 
+  //INI MERUPAKAN CONOTH CONSTRUCTUR
   Person(String paramName, String paramAddress) {
     name = paramName;
     address = paramAddress;
