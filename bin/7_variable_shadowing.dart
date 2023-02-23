0ce6613cd369c9403c6ab6c22d9c9c34a8722f64
ImranMuhamadR <imranmuhamadrafi7@gmail.com>
2023-02-23 16:19:38 +0700

    Merubah beberapa komentar pada file variable_shadowing.dart

--- bin/7_variable_shadowing.dart
+++ bin/7_variable_shadowing.dart
@@ -2,7 +2,7 @@
 !VARIABLE SHADOWING
 - variable shadowing adalah kejadian ketika kita membuat nama variable dengan nama yang sama di scope 
   yang menutupi variable dengan nama yang sama di scope diatasnya
-- Ini biasa terjadi seperti kita membuat nama parameter di method sama dengan nama field di class
+- Ini biasa terjadi seperti kita membuat nama parameter di method sama dengan nama properties di class
 - Saat terjadi variable shadowing, maka secara otomatis variable di scope diatasnya tidak bisa diakses
  */
 
@@ -12,6 +12,7 @@ class Person {
   String? address;
   final String country = "Indonesia";
 
+  //INI MERUPAKAN CONTOH DARI VARIABLE SHADOWING
   Person(String name, String address) {
     name = name;
     address = address;
