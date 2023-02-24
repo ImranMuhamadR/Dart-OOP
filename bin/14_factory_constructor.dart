981ecb112aa465956b28dde024bf6ee985f50786
ImranMuhamadR <imranmuhamadrafi7@gmail.com>
2023-02-24 19:47:30 +0700

    Membuat fila baru factory_constructor.dart dan menambahkan beberapa file di dalamnya

---
+++ bin/14_factory_constructor.dart
@@ -0,0 +1,18 @@
+/*
+!FACTORY CONSTRUCTOR
+- Factory Constructor ad alah sebuah fitur dimana kita bisa membuat constructor untuk membuat object baru, namun logika cara pembuatan object barunya bisa kita sesuaikan
+  dengan yg kita inginkan
+- Misalkan kita ingin mengembalikan object yang sama berkali-kali, kita bisa menggunakan Factory Constructor
+- Untuk membuat Factory Constructor kita bisa menggunakan kta kunci Factory sebelum pembuatan Constructor nya
+ */
+class Database {
+  Database() {
+    print('Create new Database');
+  }
+
+  static Database database = Database();
+
+  factory Database.get() {
+    return database;
+  }
+}
