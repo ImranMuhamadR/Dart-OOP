4fa7c60e3f9cf930e0e6b824e2ed8af602e3faea
ImranMuhamadR <imranmuhamadrafi7@gmail.com>
2023-02-24 09:13:10 +0700

    Membuat file baru redirecting_constructor dan menambahkan beberapa file didalamnya

--- bin/11_redirecting_constructor.dart
+++ bin/11_redirecting_constructor.dart
@@ -5,3 +5,28 @@
   memanggil this(parameter), dimana this() disini adalah dianggap menagkses Default Constructor
 - Saat membuat Redirecting Constructor, kita tidak bisa menambahkan body pada Redirecting Constructor
  */
+class Person {
+  String name = "Imran Muhamad Rafi";
+  String? address;
+  final String country = "Indonesia";
+
+  Person(this.name, this.address);
+  //INI MERUPAKAN REDIRECTING CONTRUCTOR.
+  Person.withName(String name) : this(name, "NO ADDRESS");
+  Person.withAddress(String address) : this("NO NAME", address);
+}
+
+void main() {
+  //INI MERUPAKAN DEFAULT CONSTRUCTOR
+  var person1 = Person("Imran", "Cirebon");
+  print(person1.name);
+  print(person1.address);
+
+  var person2 = Person.withName("Tatang");
+  print(person2.name);
+  print(person2.address);
+
+  var person3 = Person.withAddress("Andi");
+  print(person3.name);
+  print(person3.address);
+}
