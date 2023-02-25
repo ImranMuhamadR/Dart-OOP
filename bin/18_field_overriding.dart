5fc2dc2dfc48a5dc4b63e623acf4677f0ac7a269
ImranMuhamadR <imranmuhamadrafi7@gmail.com>
2023-02-25 21:53:45 +0700

    Menambahkan beberapa file di dalamnya

---
+++ bin/18_field_overriding.dart
@@ -0,0 +1,25 @@
+/*
+!FIELD OVERRIDING
+- Field Overriding adalah kemampuan mendeklarasikan ulang field di child class, yang sudah ada di parent class
+- Saat kita melakukan proses overriding tersebut, secara otomatis ketika kita membuat object dari class child, field yang di class parent tidak bisa di akases lagi
+- Saat melakukan field overriding, kita harus pastikan deklarasi field nya harus sama dengan di parent classnya.
+ */
+class Person {
+  String nama = "Person";
+
+  void sayHello(String name) {
+    print('Hello $nama, my name is $this.name');
+  }
+}
+
+class OtherPerson extends Person {
+  String nama = "Other Person";
+}
+
+void main() {
+  var person = Person();
+  person.sayHello('Budi');
+
+  var otherPerson = OtherPerson();
+  person.sayHello('Budi');
+}
