f5f83645d9fedae1ba96f32b91ff9b20071128a1
ImranMuhamadR <imranmuhamadrafi7@gmail.com>
2023-02-25 21:28:12 +0700

    Membuat file baru dan menambahkan beberapa file di dalamnya

---
+++ bin/17_method_overriding.dart
@@ -0,0 +1,21 @@
+/*
+!METHOD OVERRIDING
+- Method Overriding adalah kemampuan mendeklarasikan ulang method di child class, yang sudah ada di parent class
+- Saat kita melakukan proses overriding tersebut, secara otomatis ketika kita membuat object dari class child, method yang di class parent tidak bisa di akses lagi
+- Saat melakukan method overriding, kita harus pastikan deklarasi methodnya harus sama dengan parent class nya
+ */
+class Manager {
+  String? name;
+
+  //INI ADALAH SEBUAH METHOD sayHello
+  void sayHello(String name) {
+    print('Hello $name, my name is ${this.name}');
+  }
+}
+
+//INI ADALAH METHOD OVVERIDING
+class Karyawan extends Manager {
+  void sayHello(String name) {
+    print('Hello $name, my name is Karyawan${this.name}');
+  }
+}
