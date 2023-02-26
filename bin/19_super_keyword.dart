4572fb73a9c71bdfaff33add4029eb06545c8226
ImranMuhamadR <imranmuhamadrafi7@gmail.com>
2023-02-26 08:09:01 +0700

    Membuat file bary yaitu 19_super_keyword.dart

---
+++ bin/19_super_keyword.dart
@@ -0,0 +1,8 @@
+/*
+!SUPER KEYWORD
+- Kadang kita ingin mengakses method yang terdapat di class parent yang sudah terlanjur kita override di class child
+- Untuk mengakses method milik class parent, kita bisa menggunakan kata kunci super
+- Sederhananya, super digunakan untuk mengakses class parent
+- Tidak hanya method, field milik parent class pun bisa kita akses menggunakan kata kunci super
+ */
+
